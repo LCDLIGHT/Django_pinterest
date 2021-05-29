2c70aeb6cdb301c53fc5a20792b72b0510531549
Her0 <blotus14@naver.com>
2021-05-29 16:34:41 +0900

    django curse 40 commit

---
+++ projectapp/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+
+class Project(models.Model):
+    image = models.ImageField(upload_to='project/', null=False)
+    title = models.CharField(max_length=20, null=False)
+    description = models.CharField(max_length=200, null=True)
+    # 언제 만들어 졌는지
+    created_at = models.DateTimeField(auto_now=True)
+
